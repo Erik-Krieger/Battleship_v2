1fb3b1f4e66f011792ee33c47aca03d0065a9f56
Erik Krieger <erikkrieger_54@yahoo.de>
2023-08-18 22:58:23 +0200

    stuff

---
+++ Battleship_v2/Utility/CellColorConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Battleship_v2.Utility
+{
+    public class CellColorConverter : IMultiValueConverter
+    {
+        object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine("Converter");
+
+            if (values[0] is DataGridCell cell && values[1] is DataRow row)
+            {
+                try
+                {
+                    string columnName = (string)cell.Column.Header;
+                    string content = row.Field<string>(columnName); // Header must be same as column name
+                    int columnIndex = cell.Column.DisplayIndex;
+
+                    if (columnIndex == 0)
+                    {
+                        cell.FontStyle = FontStyles.Italic;
+                        cell.FontWeight = FontWeights.Bold;
+                        cell.FontSize = 20;
+                        return new SolidColorBrush(Colors.LightGoldenrodYellow);
+                    }
+
+                    if (content == "w")
+                    {
+                        return new SolidColorBrush(Colors.Blue);
+                    }
+
+                    if (content == "h")
+                    {
+                        return new SolidColorBrush(Colors.Red);
+                    }
+
+                    if (content == "m")
+                    {
+                        return new SolidColorBrush(Colors.LightGray);
+                    }
+
+                    if (content == "c" || content == "b" || content == "s" || content == "d" || content == "p")
+                    {
+                        return new SolidColorBrush(Colors.Gray);
+                    }
+                    
+                }
+                catch (Exception)
+                {
+                    return new SolidColorBrush(Colors.Black); // Error! An Exception was thrown
+                }
+            }
+            return new SolidColorBrush(Colors.Green); // Something else.
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
