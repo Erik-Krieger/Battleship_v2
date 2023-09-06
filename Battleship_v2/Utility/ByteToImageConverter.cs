a226782875769c0a907c5dec72f4fe48475d9ea6
Erik Krieger <erikkrieger_54@yahoo.de>
2023-09-06 19:19:56 +0200

    ...

---
+++ Battleship_v2/Utility/ByteToImageConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Battleship_v2.Utility
+{
+    public class ByteToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is byte[] byteArray)
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = new System.IO.MemoryStream(byteArray);
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+                return image;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Battleship_v2/ViewModels/PlayingFieldViewModel.cs
+++ Battleship_v2/ViewModels/PlayingFieldViewModel.cs
@@ -3,12 +3,9 @@ using Battleship_v2.Services;
 using Battleship_v2.Ships;
 using System.Collections.Generic;
 using System.Data;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Data;
 
 namespace Battleship_v2.ViewModels
 {
@@ -17,6 +14,17 @@ namespace Battleship_v2.ViewModels
         public PlayingFieldModel Model { get; set; }
         public PlayerType Owner { get; }
         private GameManagerService m_GameManager;
+
+        public DataGrid DataGrid
+        {
+            get => m_DataGrid;
+            set
+            {
+                m_DataGrid = value;
+                NotifyPropertyChanged(nameof(Model));
+            }
+        }
+        private DataGrid m_DataGrid;
 
         public DataTable Grid
         {
@@ -38,9 +46,9 @@ namespace Battleship_v2.ViewModels
 
                 // Check if the Column or the Item is null, in that case termiante.
                 if (m_CurrentCell.Column is null || m_CurrentCell.Item is null) return;
-                
+
                 // Call the processing Method in the Model.
-                Model.GridCellClicked(m_CurrentCell.Column.DisplayIndex, ((DataRowView)m_CurrentCell.Item)[0]);
+                Model.GridCellClicked(m_CurrentCell.Column.DisplayIndex, DataGrid.Items.IndexOf(((DataRowView)m_CurrentCell.Item)));
             }
         }
         private DataGridCellInfo m_CurrentCell;
@@ -50,11 +58,97 @@ namespace Battleship_v2.ViewModels
         public PlayingFieldViewModel(PlayerType theOwner, List<ushort> theShipList)
         {
             Owner = theOwner;
+
+            DataGrid = createDataGrid();
+
+            Grid = createDataTable();
+
+            // Setting the source
+            DataGrid.BeginInit();
+            DataGrid.ItemsSource = Grid.DefaultView;
+            DataGrid.EndInit();
+
             Model = new PlayingFieldModel(this, theOwner == PlayerType.You, theShipList);
-            if (theOwner == PlayerType.You)
+        }
+
+        private DataGrid createDataGrid()
+        {
+            // Defining sizes.
+            var starLength = new DataGridLength(1, DataGridLengthUnitType.Star);
+            double aHeaderSize = 50;
+
+            // Create a new DataGrid Object
+            var aDataGrid = new DataGrid();
+
+            // General config
+            aDataGrid.AutoGenerateColumns = false;
+            aDataGrid.VerticalScrollBarVisibility = ScrollBarVisibility.Disabled;
+            aDataGrid.HorizontalScrollBarVisibility = ScrollBarVisibility.Disabled;
+            aDataGrid.
+
+            // Header config
+            aDataGrid.HeadersVisibility = DataGridHeadersVisibility.All;
+            aDataGrid.RowHeaderWidth = aHeaderSize;
+            aDataGrid.ColumnHeaderHeight = aHeaderSize;
+
+            // Row configs
+            aDataGrid.CanUserAddRows = false;
+            aDataGrid.CanUserDeleteRows = false;
+            aDataGrid.CanUserResizeRows = false;
+            aDataGrid.RowHeight = starLength.DesiredValue;
+
+            // Column configs
+            aDataGrid.ColumnWidth = starLength;
+            aDataGrid.CanUserReorderColumns = false;
+            aDataGrid.CanUserResizeColumns = false;
+
+            // Event Handlers
+            aDataGrid.LoadingRow += DataGrid_LoadingRow;
+
+            // Add DataColumns to the DataGrid
+            for (char aLetter = 'A'; aLetter <= 'J'; aLetter++)
             {
-                //Model.DrawAllShips();
+                // Creating a new Image element.
+                FrameworkElementFactory image = new FrameworkElementFactory(typeof(Image));
+                // Set the new Binding
+                image.SetBinding(Image.SourceProperty, new Binding($"{aLetter}"));
+
+                // Create a new DataGridTemplateColumn
+                DataGridColumn column = new DataGridTemplateColumn
+                {
+                    CellTemplate = new DataTemplate() { VisualTree = image },
+                    Header = aLetter,
+                };
+                // Add the column
+                aDataGrid.Columns.Add(column);
             }
+
+            return aDataGrid;
+        }
+
+        private static DataTable createDataTable(string theDefaultValue = "../../Resources/dev_art/blue.png")
+        {
+            var aTable = new DataTable();
+
+            for (char aLetter = 'A'; aLetter <= 'J'; aLetter++)
+            {
+                DataColumn col = new DataColumn($"{aLetter}");
+                col.DefaultValue = theDefaultValue;
+                aTable.Columns.Add(col);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                DataRow row = aTable.NewRow();
+                aTable.Rows.Add(row);
+            }
+
+            return aTable;
+        }
+
+        public void DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
         }
     }
 }
