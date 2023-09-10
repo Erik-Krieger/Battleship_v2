ccd55db05e2a51b2754c4471df442bee4be78acc
Erik Krieger <erikkrieger_54@yahoo.de>
2023-09-10 14:20:27 +0200

    Added Sound Menu (It just doesn't work though)

---
+++ Battleship_v2/ViewModels/AudioControlViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Battleship_v2.Services;
+using Battleship_v2.Utility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Battleship_v2.ViewModels
+{
+    public class AudioControlViewModel : BaseViewModel
+    {
+        public Visibility WindowVisibility
+        {
+            get => m_WindowVisibility;
+            set
+            {
+                m_WindowVisibility = value;
+                NotifyPropertyChanged(nameof(WindowVisibility));
+            }
+        }
+        private Visibility m_WindowVisibility = Visibility.Visible;
+
+        public AudioControlViewModel() { }
+
+        public ICommand CmdPButtonPressed
+        {
+            get => m_CmdPButtonPressed ?? new CommandHandler(() => Debug.WriteLine("Pressed"));
+            /*get => m_CmdPButtonPressed ?? new CommandHandler(
+                () =>
+                {
+                    WindowVisibility = WindowVisibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
+                    Debug.WriteLine("Changed Visibility");
+                }
+            );*/
+        }
+        private ICommand m_CmdPButtonPressed;
+
+        public ICommand CmdOpenMultiplayerMenu
+        {
+            get => m_CmdOpenMultiplayerMenu ?? new CommandHandler(() => WindowManagerService.OpenMultiplayerMenu());
+        }
+        private ICommand m_CmdOpenMultiplayerMenu;
+    }
+}
---
+++ Battleship_v2/Views/AudioControlView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Battleship_v2.ViewModels;
+using System.Windows.Controls;
+
+namespace Battleship_v2.Views
+{
+    /// <summary>
+    /// Interaction logic for AudioControlView.xaml
+    /// </summary>
+    public partial class AudioControlView : UserControl
+    {
+        public AudioControlView()
+        {
+            InitializeComponent();
+            DataContext = new AudioControlViewModel();
+        }
+    }
+}
